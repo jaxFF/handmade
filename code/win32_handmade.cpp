839f72c2473094370c91ea10d914b448c51467d1
jaxne <jaxne@jaxne.xyz>
2020-04-07 21:39:31 -0600

    Day 3

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,5 +1,44 @@
 #include <windows.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
+
+// todo(jax): This is global for now
+global_variable bool Running;
+global_variable	BITMAPINFO BitmapInfo;
+global_variable void* BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+// Resize or initalize a Device Independent Buffer
+internal void Win32ResizeDIBSection(int Width, int Height) {
+	// todo(jax): Bulletproof this.
+	// Maybe don't free first, free after, then free first if that fails.
+
+	if (BitmapHandle) {
+		DeleteObject(BitmapHandle);
+	} 
+
+	if (!BitmapDeviceContext) {
+		// todo(jax): Should we release these under certain special circumstances?
+		BitmapDeviceContext = CreateCompatibleDC(0);
+	}
+
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = Width;
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height) {
+	StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height, BitmapMemory, &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
+
 LRESULT CALLBACK MainWindowCallback(HWND Instance, UINT Message, WPARAM wParam, LPARAM lParam) {
 	LRESULT Result = 0;
 
@@ -8,12 +47,22 @@ LRESULT CALLBACK MainWindowCallback(HWND Instance, UINT Message, WPARAM wParam, 
 	} break;
 
 	case WM_SIZE: {
+		RECT Rect;
+		GetClientRect(Instance, &Rect);
+
+		int Width = Rect.right - Rect.left;
+		int Height = Rect.bottom - Rect.top;
+		Win32ResizeDIBSection(Width, Height);
 	} break;
 
 	case WM_DESTROY: {
+		// todo(jax): Handle this as an error -- recreate window?? how about swapping renderers mid game?
+		Running = false;
 	} break;
 
 	case WM_CLOSE: {
+		// todo(jax): Handle this with a message to user
+		Running = false;
 	} break;
 
 	case WM_ACTIVATEAPP: {
@@ -28,9 +77,7 @@ LRESULT CALLBACK MainWindowCallback(HWND Instance, UINT Message, WPARAM wParam, 
 		int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 		int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
 
-		static DWORD Op = WHITENESS;
-		PatBlt(DeviceContext, X, Y, Width, Height, Op);
-		Op = (Op == WHITENESS) ? BLACKNESS : WHITENESS;
+		Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
 
 		EndPaint(Instance, &Paint);
 	} break;
@@ -44,8 +91,6 @@ LRESULT CALLBACK MainWindowCallback(HWND Instance, UINT Message, WPARAM wParam, 
 }
 
 int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int nCmdShow) {
-	MessageBox(0, "Is this thing working?", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
-
 	WNDCLASS WindowClass = {};
 
 	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
@@ -68,7 +113,8 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 			0);
 
 		if (WindowHandle) {
-			for (;;) {
+			Running = true;
+			while (Running) {
 				MSG Message;
 				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
 				if (MessageResult > 0) {
